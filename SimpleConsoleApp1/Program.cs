3d952aed2d1c057c914a8d7ea969d11ea9b84667
dowdeswells <dowdeswells@yahoo.com.au>
2016-11-14 15:02:18 +1100

    first commit

--- SimpleConsoleApp1/Program.cs
+++ SimpleConsoleApp1/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 
@@ -12,13 +14,25 @@ namespace SimpleConsoleApp1
 {
     class Program
     {
+        private const string StorageAccountConnectionString = "StorageAccountConnectionString";
         static void Main(string[] args)
         {
-            var queueUtilities = new QueueUtilities("");
-            var queueClient = queueUtilities.NewCloudQueueClient("queue");
-            queueClient.CreateIfNotExists();
-            var message = args.Any() ? args[0] : "PING";
-            queueUtilities.QueueSimpleMessage("queue", message);
+            try
+            {
+                var message = args.Any() ? args[0] : "PING";
+                Trace.TraceInformation("Message: {0}", message);
+                var connectionString = CloudConfigurationManager.GetSetting(StorageAccountConnectionString);
+                Trace.TraceInformation("Connection String: {0}", connectionString);
+
+                var queueUtilities = new QueueUtilities(connectionString);
+                var queueClient = queueUtilities.NewCloudQueueClient("queue");
+                queueClient.CreateIfNotExists();
+                queueUtilities.QueueSimpleMessage("queue", message);
+            }
+            catch (Exception exc)
+            {
+                Trace.TraceError(exc.ToString());
+            }
         }
 
     }
